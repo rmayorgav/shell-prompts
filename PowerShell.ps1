3768fdff6a66d495ff23ed7d5b1b602af3339323
RMayorgaV <rmayorgav@rmayorgav.com>
2022-06-22 14:10:41 -0400

    PowerShell: Init

---
+++ PowerShell.ps1
@@ -0,0 +1,6 @@
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.2
+
+function prompt {
+  $currentPath = Split-Path -leaf -path (Get-Location)
+  "$env:USERNAME@$env:COMPUTERNAME [$currentPath]> "
+}
