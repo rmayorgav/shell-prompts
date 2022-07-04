f6fedf2c098e2fa2e591037c02fb56d4154de0eb
Rodrigo Mayorga V <rmayorgav@rmayorgav.com>
2022-07-04 17:44:45 -0400

    Update PowerShell.ps1

--- PowerShell.ps1
+++ PowerShell.ps1
@@ -1,6 +1,29 @@
-# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.2
+function Write-BranchName () {
+	# https://stackoverflow.com/questions/1287718/how-can-i-display-my-current-git-branch-name-in-my-powershell-prompt
+    try {
+        $branch = git rev-parse --abbrev-ref HEAD
+
+        if ($branch -eq "HEAD") {
+            $branch = git rev-parse --short HEAD
+            Write-Host " ($branch)" -ForegroundColor "red"
+        }
+        else {
+            Write-Host " ($branch)" -ForegroundColor "blue"
+        }
+    } catch {
+        Write-Host " (no branches yet)" -ForegroundColor "yellow"
+    }
+}
 
 function prompt {
-  $currentPath = Split-Path -leaf -path (Get-Location)
-  "$env:USERNAME@$env:COMPUTERNAME [$([char]27)[32m$currentPath$([char]27)[m]> "
+  $currentPath = Get-Location
+  $userPrompt = "$('>' * ($nestedPromptLevel + 1)) "
+  #Write-Host "$env:USERNAME@$env:COMPUTERNAME " -NoNewLine
+  Write-Host "$currentPath" -NoNewLine -ForegroundColor "green"
+  if (Test-Path .git) {
+	  Write-BranchName
+  } else {
+	  Write-Host 
+  }
+  return $userPrompt
 }
