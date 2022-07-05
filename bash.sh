e7b0a5f08469109d45602f36297a7fb2a90af761
RMayorgaV <rmayorgav@rmayorgav.com>
2022-07-05 17:32:59 -0400

    Bash: Update colors

--- bash.sh
+++ bash.sh
@@ -2,4 +2,4 @@ parse_git_branch() {
     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
 }
 
-export PS1="\[\033[34;1m\]\u@\h\[\033[0m\] \[\033[32m\]\w\[\033[0m\]\[\033[33m\]\$(parse_git_branch)\[\033[00m\]\n$ "
+export PS1="\[\033[32m\]\u@\h\[\033[0m\] \[\033[33m\]\w\[\033[0m\]\[\033[36m\]\$(parse_git_branch)\[\033[00m\]\n$ "
